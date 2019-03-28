30b6065907d591fd1a30e76b86a9fb6925d89980
lihuan <lihuan@aiyunxiao.com>
2019-03-28 16:18:05 +0800

    vue js

--- client/jest.config.js
+++ client/jest.config.js
@@ -3,14 +3,12 @@ module.exports = {
     'js',
     'jsx',
     'json',
-    'vue',
-    'ts',
-    'tsx'
+    'vue'
   ],
   transform: {
     '^.+\\.vue$': 'vue-jest',
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.jsx?$': 'babel-jest'
   },
   transformIgnorePatterns: [
     '/node_modules/'
@@ -24,10 +22,5 @@ module.exports = {
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
-  testURL: 'http://localhost/',
-  globals: {
-    'ts-jest': {
-      babelConfig: true
-    }
-  }
+  testURL: 'http://localhost/'
 }
