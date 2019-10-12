06e13db4788c6dc93effc7d14097087fe959a918
lihuan <lihuan@aiyunxiao.com>
2019-10-12 16:23:18 +0800

    add hook demo

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,42 @@ import React, { Component } from 'react'
 import './App.scss'
 import { Layout } from 'antd'
 import Home from './views/home/home'
-
+import HookGame from './views/hook-game/hook-game';
+interface AppState {
+  tabName: String
+}
 const { Header, Content, Footer, Sider } = Layout
 
-class App extends Component {
+class App extends Component<{}, AppState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tabName: "hook-game"
+    }
+  }
+  changeTab(name: String) {
+
+    this.setState({
+      tabName: this.state.tabName === 'hook-game' ? 'chess' : 'hook-game'
+    });
+    console.log(this.state.tabName);
+  }
   render() {
     return (
       <div className="App">
         <Layout className="container">
-          <Header className="header">perceive</Header>
+          <Header className="header">
+            <span onClick={() => this.changeTab('hook-game')}>perceive</span>
+            </Header>
           <Layout className="content">
             <Sider className="sider">导航</Sider>
             <Content className="main">
-              <Home name={'井字棋'}></Home>
+              {
+                this.state.tabName === 'hook-game' && <HookGame></HookGame>
+              }
+              {
+                this.state.tabName === 'chess' && <Home name={'井字棋'}></Home>
+              }
             </Content>
           </Layout>
           <Footer className="footer">关于：测试阶段</Footer>
---
+++ src/views/hook-game/hook-game.tsx
@@ -0,0 +1,16 @@
+import './hook-game.scss'
+
+import React, { useState } from 'react';
+
+export default function HookGame() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+      <div className="content"></div>
+    </div>
+  );
+}
