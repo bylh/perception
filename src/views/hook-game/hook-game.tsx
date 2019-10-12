5481c164fd129131fb4a29186a2fbc15eecbc54f
lihuan <lihuan@aiyunxiao.com>
2019-10-12 19:59:21 +0800

    完善页面

--- src/views/hook-game/hook-game.tsx
+++ src/views/hook-game/hook-game.tsx
@@ -16,59 +16,62 @@ export default function HookGame() {
     // 创建场景
 
     // createScene();
-
-    let WIDTH = window.innerWidth, HEIGHT = window.innerHeight;
-    // TODO 此函数中获取高度不一致
-    let content = document.getElementById('hook-game-content');
-    const { clientWidth, clientHeight } = content;
-    console.log(clientWidth, clientHeight, content);
-    WIDTH = clientWidth;
-    HEIGHT = clientHeight
-
-    let scene = new THREE.Scene();
-    let camera = new THREE.PerspectiveCamera(75, HEIGHT / WIDTH, 0.1, 1000);
-    let renderer = new THREE.WebGLRenderer();
-    renderer.setSize(WIDTH, HEIGHT);
-    content.appendChild(renderer.domElement);
-    // document.body.appendChild();
-    let geometry = new THREE.BoxGeometry(1, 1, 1);
-    let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    let cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    camera.position.z = 5;
-    let animate = function () {
-      requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render(scene, camera);
-    };
-    animate();
-    const handleWindowResize = () => {
-      // 更新渲染器的高度和宽度以及相机的纵横比
-      // HEIGHT = window.innerHeight;
-      // WIDTH = window.innerWidth;
+    setTimeout(() => {
+      let WIDTH = window.innerWidth, HEIGHT = window.innerHeight;
+      // TODO 此函数中获取高度不一致
       let content = document.getElementById('hook-game-content');
       const { clientWidth, clientHeight } = content;
+      console.log(clientWidth, clientHeight, content, content.offsetWidth, content.offsetHeight);
       WIDTH = clientWidth;
       HEIGHT = clientHeight
-      console.log(WIDTH, HEIGHT);
+
+      let scene = new THREE.Scene();
+      let camera = new THREE.PerspectiveCamera(75, HEIGHT / WIDTH, 0.1, 1000);
+      let renderer = new THREE.WebGLRenderer();
       renderer.setSize(WIDTH, HEIGHT);
-      camera.aspect = WIDTH / HEIGHT;
-      camera.updateProjectionMatrix();
-    };
-    // 监听屏幕，缩放屏幕更新相机和渲染器的尺寸
-    window.addEventListener('resize', handleWindowResize, false);
+      content.appendChild(renderer.domElement);
+      // document.body.appendChild();
+      let geometry = new THREE.BoxGeometry(1, 1, 1);
+      let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      let cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
+      camera.position.z = 5;
+      let animate = function () {
+        requestAnimationFrame(animate);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        renderer.render(scene, camera);
+      };
+      animate();
+      const handleWindowResize = () => {
+        // 更新渲染器的高度和宽度以及相机的纵横比
+        // HEIGHT = window.innerHeight;
+        // WIDTH = window.innerWidth;
+        let content = document.getElementById('hook-game-content');
+        const { clientWidth, clientHeight } = content;
+        WIDTH = clientWidth;
+        HEIGHT = clientHeight
+        console.log(WIDTH, HEIGHT);
+        renderer.setSize(WIDTH, HEIGHT);
+        camera.aspect = WIDTH / HEIGHT;
+        camera.updateProjectionMatrix();
+      };
+      // 监听屏幕，缩放屏幕更新相机和渲染器的尺寸
+      window.addEventListener('resize', handleWindowResize, false);
+    }, 0);
   }, []);
 
   return (
     <div className="container">
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
+      <div className="container-header">
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me
       </button>
+      </div>
       <div
         id="hook-game-content"
-        className="content"></div>
+        className="container-content"></div>
     </div>
   );
 }
