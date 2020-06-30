3ffbbe2ed454f4f38e1304b2f82fc2d56a6cd34a
lihuan <494397353@qq.com>
2020-06-30 10:54:01 +0800

    done： add soul component

--- src/views/carousel/carousel.tsx
+++ src/views/carousel/carousel.tsx
@@ -1,29 +1,39 @@
-import React from 'react';
-import { Carousel, Card} from 'antd'
+import React, {useState, useEffect} from 'react';
+import axios from '../../service/axios'
+import { Carousel} from 'antd'
 import './carousel.scss'
-const { Meta } = Card
+interface Soul {
+    title: string,
+    hits: string
+}
+
 export default function Search() {
+    const [souls, setSouls] = useState<Array<Soul>>([])
+    useEffect(() => {
+        
+        const fetchData = async () => {
+            const res = await axios.request({
+                url: '/souls',
+                params: {
+                    offset: -1,
+                    limit: 4
+                }
+            })
+            if (res.data.code !== 200) {
+                return;
+            }
+            setSouls(res.data.data || [])
+        }
+        fetchData().then().catch(err => {
+            console.error('获取souls失败', err)
+        })
+    }, [])
     return (
         <div className="carousel">
-            <Carousel autoplay>
-                <div>
-                    <Card
-                        hoverable
-                        // style={{ width: 240 }}
-                        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-                    >
-                        <Meta title="Europe Street beat" description="www.instagram.com" />
-                    </Card>
-                </div>
-                <div>
-                    <h3>2</h3>
-                </div>
-                <div>
-                    <h3>3</h3>
-                </div>
-                <div>
-                    <h3>4</h3>
-                </div>
+            <Carousel effect="scrollx" autoplay>
+                {
+                    souls.map((item, index) => <div key={index}><div className="soul-item">{item.title}</div></div>)
+                }
             </Carousel>
         </div>
     );
